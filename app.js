207358ebd4fe414d5ec22a14157700a9ae49a60f
Andrea Wu <lixianhyun@gmail.com>
2020-09-27 12:51:49 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const request = require("request");
+const data = {
+    url : "https://api.pexels.com/v1/search?query=cake&per_page=2",
+    headers: {
+      'Authorization': '563492ad6f917000010000013fb745a9edf242cb838bef82e288a017'
+    } 
+};
+
+app.set("view engine","ejs");
+
+app.get("/", function(req, res){
+    res.send("Hi There!!!");
+});
+
+request(data, function(error,response, body){
+ if(!error && response.statusCode == 200){
+      console.log(body);
+  }
+});
+
+app.listen(3000, function(){
+    console.log("Server Running");
+});
+

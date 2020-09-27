50c9185650811ea119fa02d8cfcf677ebd8ab922
Andrea Wu <lixianhyun@gmail.com>
2020-09-27 14:07:51 -0700

    adding images/photographer to main.ejs from Pexel Api's database

--- app.js
+++ app.js
@@ -6,42 +6,23 @@ app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-    res.render("main");
+    const query = "flower";
+    const data = {
+        url : "https://api.pexels.com/v1/search/?page=2\u0026per_page=50\u0026query=" + query,
+        headers: {
+        'Authorization': '563492ad6f917000010000013fb745a9edf242cb838bef82e288a017'
+        } 
+    };
+    request(data, function(error,response, body){
+        if(!error && response.statusCode == 200){
+            const info = JSON.parse(body);
+            // console.log("info: " + info);
+            // console.log("body: " + body);
+            res.render("main", {info});
+        };
+    });
 });
-
-const query = "flower";
-const data = {
-    url : "https://api.pexels.com/v1/search/?page=2\u0026per_page=2\u0026query=" + query,
-    headers: {
-      'Authorization': '563492ad6f917000010000013fb745a9edf242cb838bef82e288a017'
-    } 
-};
-
-request(data, function(error,response, body){
-    if(!error && response.statusCode == 200){
-        const details = JSON.parse(body);
-        // console.log("details: " + details.url);
-        // console.log("body: " + body);
-     };
-   });
-
-// app.post("/results/:id", function(req, res){
-//     const query = req.query.search.toLowerCase().trim();
-//     const data = {
-//         url : "https://api.pexels.com/v1/search?query=cake&per_page=2",
-//         headers: {
-//           'Authorization': '563492ad6f917000010000013fb745a9edf242cb838bef82e288a017'
-//         } 
-//     };
-
-//     request(data, function(error,response, body){
-//         if(!error && response.statusCode == 200){
-//             console.log(body);
-//          }
-//        });
-// });
 
 app.listen(3000, function(){
     console.log("Server Running");
 });
-

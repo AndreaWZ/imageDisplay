78b1a3c7d3de1bcc7908b5ddc9d4b03af924fef7
Andrea Wu <lixianhyun@gmail.com>
2020-09-27 20:36:24 -0700

    add thumbnail to images and add DB pexels logo

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-    const query = "flower";
+    const query = "spring";
     const data = {
-        url : "https://api.pexels.com/v1/search/?page=2\u0026per_page=20\u0026query=" + query,
+        url : "https://api.pexels.com/v1/search/?page=2\u0026per_page=30\u0026query=" + query,
         headers: {
         'Authorization': '563492ad6f917000010000013fb745a9edf242cb838bef82e288a017'
         } 

2ddb455aaf6a890a04e744afcc0fdf529cd0341c
Andrea Wu <lixianhyun@gmail.com>
2020-09-30 00:57:47 -0700

    made functional the searching bar and displaying searching images on the results template page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express"),
       app = express(),
       request = require("request");
 
+const apiKey = "563492ad6f917000010000013fb745a9edf242cb838bef82e288a017";
+
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
 
@@ -10,7 +12,7 @@ app.get("/", function(req, res){
     const data = {
         url : "https://api.pexels.com/v1/search/?page=2\u0026per_page=30\u0026query=" + query,
         headers: {
-        'Authorization': '563492ad6f917000010000013fb745a9edf242cb838bef82e288a017'
+        'Authorization': apiKey
         } 
     };
     request(data, function(error,response, body){
@@ -23,8 +25,20 @@ app.get("/", function(req, res){
     });
 });
 
-app.post("/:name/results", function(req, res){
-    res.render("results");
+app.get("/results", function(req, res){
+    const query = req.query.search;
+    const data = {
+        url : "https://api.pexels.com/v1/search/?page=2\u0026per_page=30\u0026query=" + query,
+        headers: {
+        'Authorization': apiKey
+        } 
+    };
+    request(data, function(error,response, body){
+        if(!error && response.statusCode == 200){
+            const results = JSON.parse(body);
+            res.render("results", {results, query});
+        };
+    });
 });
 
 app.listen(3000, function(){
